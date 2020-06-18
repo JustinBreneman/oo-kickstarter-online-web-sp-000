93a1b830f18a3e78904c7454b93fdbdfc8b42e7a
Justin Breneman <justin.breneman@yahoo.com>
2020-06-18 23:57:39 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,6 +6,8 @@ class Backer
   end
   def back_project(title)
     @backed_projects << title
-    title.add_backer(self)
+    if title.backers.include?(self) == false
+      title.add_backer(self)
+    end
   end
 end
